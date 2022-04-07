f1279c8fbf252a104a08c07e656962148f528507
darkgod1kandarp <009kandarp@gmail.com>
2022-04-07 15:47:19 +0530

    mine block

--- block.js
+++ block.js
@@ -24,6 +24,16 @@ class Block {
         //will call same class 
         return new this('Genesis time','----','f1r57-h45h','Genesis data');
     }
+
+    //after genesis block other all block are added
+    static mineBlock(lastBlock,data){
+      
+        const lastHash = lastBlock.hash;
+        const timestamp = Date.now();
+       
+        const hash = 'Todo-hash';
+        return new this(timestamp,lastHash,hash,data);
+    }
 }
 
 module.exports = Block;
