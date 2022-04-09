e23d2cf8c48483923c9891f8cf6d4f70beab2716
darkgod1kandarp <009kandarp@gmail.com>
2022-04-10 00:57:53 +0530

    describe can not be used in nodemon you have to use mocha and then put the file location from the node modules in test part of package.json

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
 const Block =  require("./block");
+var expect = require('chai').expect;
 
- const fooBlock =  Block.mineBlock(Block.genesis(),'foo');
- console.log(fooBlock.toString());   
+//break the test into components 
+describe("Block", () => {
+     let data , prevBlock , block;
+
+     //initialize before every test case
+     beforeEach(() => {
+         data = 'foo';
+        prevBlock = Block.genesis();
+        block = Block.mineBlock(prevBlock, data);
+     })
+
+     //first test case 
+     it('sets the data to match the input', () => {
+         expect(block.data).equal(data);
+     })
+
+     //second test case
+     it('sets the previous hash to match  input block lasthash', () => {
+         expect(block.lastHash).equal(prevBlock.hash);
+     })
+ });
