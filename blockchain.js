d857b9716959ce7c43b85556649fc80d11c79337
darkgod1kandarp <009kandarp@gmail.com>
2022-04-10 11:42:32 +0530

    chain validation done

--- blockchain.js
+++ blockchain.js
@@ -9,6 +9,26 @@ class Blockchain {
     this.chain.push(block);
     return block;
   }
+
+  isChainValidate(chain){
+
+    //You can not directly compare two dictionary in javascript it will 
+    //always show false so use JSON.stringify() that convert collection to the string
+      if (JSON.stringify(chain[0])!==JSON.stringify(Block.genesis())) return false;
+
+      //checking the hash value and its genertaed hash value from teh block
+      var i;
+      for ( i = 1; i < chain.length;i++) {
+          const block = chain[i];
+            const lastBlock = chain[i - 1];
+            if(block.lastHash!==lastBlock.hash || block.hash!==Block.blockHash(block)){
+                return false;
+            }
+
+          
+      }
+      return true;
+  }
 }
 
 module.exports = Blockchain;
--- src/blockchain.test.js
+++ src/blockchain.test.js
@@ -1,28 +1,47 @@
-const Blockchain = require('../blockchain');
-const Block = require('../block');
-const { expect } = require('chai');
+const Blockchain = require("../blockchain");
+const Block = require("../block");
+const { expect } = require("chai");
 
 //collection of all the test cases
 describe("Blockchain", () => {
-    let blockchain;
+  let blockchain;
+  let blockchain2;
 
-    //this block will run after each test case
-    beforeEach(() => {
-       blockchain = new Blockchain();
-    })
+  //this block will run after each test case
+  beforeEach(() => {
+    blockchain = new Blockchain();
+    blockchain2 = new Blockchain();
+  });
 
-    it("start with genesis block", () => {
+  it("start with genesis block", () => {
+    //for comparing the collection you have to use deep.equal not only equal
+    expect(blockchain.chain[0]).to.deep.equal(Block.genesis());
+  });
+  it("add a new block", () => {
+    //adding data block to the chain
+    const data = "foo";
+    blockchain.addBlock(data);
 
-        //for comparing the collection you have to use deep.equal not only equal 
-        expect(blockchain.chain[0]).to.deep.equal(Block.genesis());
-    })
-    it("add a new block",()=>{
+    //checking if the data has been added or not
+    expect(blockchain.chain[blockchain.chain.length - 1].data).equal(data);
+  });
 
-        //adding data block to the chain
-        const data = "foo";
-        blockchain.addBlock(data);
+  it("chain is validate", () => {
+    //creating chain
+    const data = "kandarp";
+    blockchain2.addBlock(data);
 
-         //checking if the data has been added or not
-         expect(blockchain.chain[blockchain.chain.length - 1].data).equal(data);
-    })
+    //checking if the chain is valid or not(genesis one)
+    expect(blockchain2.isChainValidate(blockchain2.chain)).to.be.true;
+  });
+  it("chain is not validate", () => {
+    //creating chain
+    const data = "kandarp";
+    blockchain2.addBlock(data);
+
+    //changing the chain
+    blockchain2.chain[1].data = "kan";
+    //checking if the chain is valid or not(genesis one)
+    expect(blockchain2.isChainValidate(blockchain2.chain)).to.be.false;
+  });
 });
